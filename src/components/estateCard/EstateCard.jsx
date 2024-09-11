a5fcdf7ee3bd67bf9d65e38c627469b5575ea68e
makinloot <itorn9777@gmail.com>
2024-09-11 21:48:25 +0400

    add listings on home page

---
+++ src/components/estateCard/EstateCard.jsx
@@ -0,0 +1,53 @@
+import Card from "antd/es/card/Card";
+import "./EstateCard.css";
+import pinIcon from "/pin.png";
+import bedIcon from "/bed.png";
+import squareMetersIcon from "/square-meters.png";
+import zipIcon from "/zip.png";
+const EstateCard = ({
+  img,
+  price,
+  address,
+  city,
+  bedrooms,
+  area,
+  zip,
+  rented,
+}) => {
+  return (
+    <Card
+      className="estate-card"
+      hoverable
+      cover={<img className="main-img" src={img} />}
+    >
+      <div className="estate-card-details">
+        <span className="estate-card-price">{price} ₾</span>
+        <div className="estate-card-row">
+          <img src={pinIcon} />
+          <span>
+            {city}, {address}
+          </span>
+        </div>
+        <div className="estate-card-col">
+          <div className="estate-card-row no-padding">
+            <img src={bedIcon} />
+            <span>{bedrooms}</span>
+          </div>
+          <div className="estate-card-row no-padding">
+            <img src={squareMetersIcon} />
+            <span>{area}</span>
+          </div>
+          <div className="estate-card-row no-padding">
+            <img src={zipIcon} />
+            <span>{zip}</span>
+          </div>
+        </div>
+      </div>
+      <span className="estate-card-rent">
+        {rented ? "ქირავდება" : "იყიდება"}
+      </span>
+    </Card>
+  );
+};
+
+export default EstateCard;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,16 +1,59 @@
-import React from 'react'
-import Categories from '../../components/categories/Categories'
+import { useEffect, useState } from "react";
+import Categories from "../../components/categories/Categories";
+import { useAppContext } from "../../context/ContextProvider";
+import EstateCard from "../../components/estateCard/EstateCard";
 
 const Home = () => {
+  const { setBaseURL, api } = useAppContext();
+  const [listings, setListings] = useState([]);
+  // fetch cities
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setBaseURL("https://api.real-estate-manager.redberryinternship.ge/api");
+        const response = await api.get("/real-estates");
+        console.log(response.data);
+        setListings(response.data);
+      } catch (error) {
+        console.error("Error fetching cities:", error);
+      }
+    };
+
+    fetchCities();
+  }, []);
+
   return (
     <div className="Home">
-        <div className="container">
-            <div className="Home-wrapper">
-                <Categories />
-            </div>
+      <div className="container">
+        <div className="Home-wrapper">
+          <Categories />
+          <div
+            style={{
+              paddingTop: 32,
+              display: "flex",
+              flexWrap: "wrap",
+              gap: 20,
+            }}
+          >
+            {listings.map((item) => (
+              <div>
+                <EstateCard
+                  address={item.address}
+                  area={item.area}
+                  bedrooms={item.bedrooms}
+                  city={item.city.name}
+                  img={item.image}
+                  price={item.price}
+                  rented={item.is_rental}
+                  zip={item.zip}
+                />
+              </div>
+            ))}
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
